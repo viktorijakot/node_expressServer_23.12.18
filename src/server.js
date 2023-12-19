0a1ecadc327ed978a149d6c0a89d5e9507b2beb7
viktorija <viktorija.kotaite@gmail.com>
2023-12-19 19:28:21 +0200

    post method update

--- src/server.js
+++ src/server.js
@@ -1,9 +1,8 @@
-"use strict";
-
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+
 const app = express();
 const port = 3000;
 
@@ -37,8 +36,10 @@ let users = [
 //Middleware
 app.use(morgan("dev"));
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+//jei nori i body gauti json
+app.use(express.json());
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
 
 //Routes
 app.get("/", (req, res) => {
@@ -84,16 +85,37 @@ app.get("/api/users/:userID", (request, response) => {
 //DELETE - /api/users/2 -delete user with id
 
 app.delete("/api/users/:usersId", (request, response) => {
-  const userId = +request.params.usersId;
-  //grazinti viska isskyrus ta el ant kurio id yra === userID
-  users = users.filter((userObj) => userId !== userObj.id);
-  response.json(users);
+  const userId = request.params.usersId;
+  console.log(request.params);
+
+  users = users.filter((userObj) => +userId !== userObj.id);
+  response.sendStatus(201);
 });
 
-app.post("/api/users", (request, response) => {
-  const data = request.body;
-  users.push(data);
-  response.send(users);
+// app.post("/api/users", (request, response) => {
+//   // const data = request.body;
+//   const data = {
+//     id: Math.random().toString().slice(3),
+//     name: request.body.name,
+//     town: request.body.town,
+//     isDriver: request.body.isDriver,
+//   };
+//   users.push(data);
+//   response.send(users);
+// });
+
+app.post("/api/users", (req, res) => {
+  // console.log('req.body ===', req.body);
+  // sukuriam nauja useri
+  const newUser = {
+    id: parseInt(Math.random().toString().slice(3)),
+    name: req.body.name,
+    town: req.body.town,
+    isDriver: req.body.isDriver,
+  };
+  console.log("newUser ===", newUser);
+  users.push(newUser);
+  res.sendStatus(201);
 });
 
 app.listen(port, () => {
